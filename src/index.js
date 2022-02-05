8aa943d5dd723fced88603c973da12b4200e8813
hitmaizer <jose.sousalves@gmail.com>
2022-02-05 15:26:10 +0000

    removed themeProvider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { ThemeProvider } from "styled-components"
 import App from "./App"
-import Theme from "./theme/theme"
+
 
 
 ReactDOM.render(
-<ThemeProvider theme={Theme}>
-    <App />
-</ThemeProvider>
+<App />
 , document.getElementById('root'))
