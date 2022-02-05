904a92bd00ba9b8956d1fbfe0f71217713f1e4f9
hitmaizer <jose.sousalves@gmail.com>
2022-02-05 13:10:51 +0000

    a button styled with sc

--- src/components/Button.styled.js
+++ src/components/Button.styled.js
@@ -5,10 +5,18 @@ export default function Button(props) {
 
     const Button = styled.button`
       padding: 1em 2em;
-      background-color: red;
+      background-color: ${props.primary ? "blue" : "red"};
       border: none;
       border-radius: 12px;
       color: #fff;
+      text-transform: uppercase;
+      cursor: pointer;
+
+      &:hover {
+          transform: translateY(-3%);
+          background-color: teal;
+      }
+
     `;
 
     return (
