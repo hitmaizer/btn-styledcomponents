518af56f9f61bc58e1215040cc117b5eea9cd486
hitmaizer <jose.sousalves@gmail.com>
2022-02-05 18:13:50 +0000

    icon render logic

--- src/elements/Icon.js
+++ src/elements/Icon.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { LocalGroceryStore } from '@styled-icons/material';
 
 export default function Icon(props) { 
-    return (
-        (props) => props.icon && <LocalGroceryStore size="24px" color="#fff" className="btn__icon"/>
-    )
+    
+        if(props.start || props.end) {
+            return <LocalGroceryStore size="24px" color="#fff" className="btn__icon"/>
+        } else {
+            return null
+        }
+    
 }
