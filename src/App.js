7b8fdcfc2de43f056f6a11fd36c0ab4034a4984f
hitmaizer <jose.sousalves@gmail.com>
2022-02-05 19:17:44 +0000

    markup + conditional renders

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react"
 import { ThemeProvider } from "styled-components"
-import Button from "./components/Button"
+import Button from "./components/Buttons"
 import Theme from "./theme/theme"
 import "./scss/styles.css"
+
+
 
 export default function App(props){
     
     return (
         <div className="page__wrapper">
             <ThemeProvider theme={Theme}>
-                <Button>Default</Button>    
+                <Button />    
             </ThemeProvider>
         </div>
     )
