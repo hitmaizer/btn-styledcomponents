21f2e5287500362566d8ebc35f8003f4add2bde2
hitmaizer <jose.sousalves@gmail.com>
2022-02-05 15:26:35 +0000

    seperate objs for different styles

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,16 +1,33 @@
 
 export default {
-    default: "#E0E0E0",
-    defaultHover: "#AEAEAE",
-    defaultText: "#3F3F3F",
-    primary: "#2962FF",
-    primaryHover: "#0039CB",
-    secondary: "#455A64",
-    secondaryHover: "#1C313A",
-    danger: "#D32F2F",
-    dangerHover: "#9A0007",
-    disabled: "#E0E0E0",
-    disabledText: "#9E9E9E",
 
-    dropShadow: "rgba(51, 51, 51, 0.2)"
+    colors: {
+        default: "#E0E0E0",
+        defaultHover: "#AEAEAE",
+        defaultText: "#3F3F3F",
+        primary: "#2962FF",
+        primaryHover: "#0039CB",
+        secondary: "#455A64",
+        secondaryHover: "#1C313A",
+        danger: "#D32F2F",
+        dangerHover: "#9A0007",
+        disabled: "#E0E0E0",
+        disabledText: "#9E9E9E",
+        outlinePrimary: "#3D5AFE",
+        outlinePrimaryHover: "rgba(41, 98, 255, 0.1)"
+    },
+
+    dropShadow: {
+        
+        dropShadow: "rgba(51, 51, 51, 0.2)",
+    },
+
+    sizes: {
+        sm: "6px 12px",
+        md: "8px 16px",
+        lg: "12px 24px"
+
+    }
+
+
 }
