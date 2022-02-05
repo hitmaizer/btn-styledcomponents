3fb5adeeb7d6dee6a5c38010b6c85059c4890296
hitmaizer <jose.sousalves@gmail.com>
2022-02-05 16:19:08 +0000

    outline logic finished + comments

--- src/components/Button.styled.js
+++ src/components/Button.styled.js
@@ -2,13 +2,14 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 
 const Btn = styled.button`
+      // comum a todos
       border: none;
       border-radius: 6px;
       box-shadow: ${(props) => props.theme.dropShadow.dropShadow};
       font-family: ${(props) => props.theme.fonts.font};
       cursor: pointer;
       
-
+      // default
       ${(props) => props.default && css `
         color: ${props.theme.colors.defaultText};
         &:hover, &:focus {
@@ -18,9 +19,17 @@ const Btn = styled.button`
         ${(props) => props.disableShadow && css `
             box-shadow: none;
         `}
+        
+        ${(props) => props.outline && css `
+            background-color: transparent;
+            border: 2px solid ${props.theme.colors.defaultText};
+            color: ${props.theme.colors.defaultText};
+        `}
+
 
       `}
       
+      // primary
       ${(props) => props.primary && css `
         background-color: ${props.theme.colors.primary};
         color: #fff;
@@ -32,8 +41,19 @@ const Btn = styled.button`
             box-shadow: none;
         `}
 
+        ${(props) => props.outline && css `
+            background-color: transparent;
+            border: 2px solid ${props.theme.colors.primary};
+            color: ${props.theme.colors.primary};
+            &:hover {
+                background-color: ${props.theme.colors.outlinePrimaryHover};
+            }
+        `}
+
+
       `}
       
+      //secondary
       ${(props) => props.secondary && css `
         background-color: ${props.theme.colors.secondary};
         color: #fff;
@@ -45,8 +65,19 @@ const Btn = styled.button`
             box-shadow: none;
         `}
 
+        ${(props) => props.outline && css `
+            background-color: transparent;
+            border: 2px solid ${props.theme.colors.secondary};
+            color: ${props.theme.colors.secondary};
+            &:hover {
+                color: white;
+                background-color: ${props.theme.colors.secondary};
+            }
+        `}
+
       `}
       
+      //danger
       ${(props) => props.danger && css `
         background-color: ${props.theme.colors.danger};
         color: #fff;
@@ -58,21 +89,36 @@ const Btn = styled.button`
             box-shadow: none;
         `}
 
+        ${(props) => props.outline && css `
+            background-color: transparent;
+            border: 2px solid ${props.theme.colors.danger};
+            color: ${props.theme.colors.danger};
+            &:hover {
+                color: white;
+                background-color: ${props.theme.colors.danger};
+            }
+        `}
+
       `}
       
+      //disabled 
       ${(props) => props.disabled && css `
         background-color: ${props.theme.colors.disabled};
         color: ${props.theme.colors.disabledText};
       `}
       
+
+      //size large
       ${(props) => props.lg && css `
         padding: ${props.theme.sizes.lg}
       `}
       
+      //size medium
       ${(props) => props.md && css `
         padding: ${props.theme.sizes.md}
       `}
       
+      //size small
       ${(props) => props.sm && css `
         padding: ${props.theme.sizes.sm}
       `}
@@ -85,7 +131,7 @@ const Btn = styled.button`
 const Button = (props) => {
 
     return (
-        <Btn primary lg>{props.children}</Btn>
+        <Btn secondary lg outline>{props.children}</Btn>
     )
 }
 
