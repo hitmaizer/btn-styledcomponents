2085fb3b92b4537c47e095e0c5a494a6fcee0bf8
hitmaizer <jose.sousalves@gmail.com>
2022-02-05 14:23:58 +0000

    import themeProvider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { ThemeProvider } from "styled-components"
 import App from "./App"
+import Theme from "./theme/theme"
 
-ReactDOM.render(<App />, document.getElementById('root'))
+
+ReactDOM.render(
+<ThemeProvider theme={Theme}>
+    <App />
+</ThemeProvider>
+, document.getElementById('root'))
