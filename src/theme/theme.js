e1185369d54b979142cb15a6e0a3c7a346ebef71
hitmaizer <jose.sousalves@gmail.com>
2022-02-05 14:23:30 +0000

    color + drop shadow variables on theme obj

---
+++ src/theme/theme.js
@@ -0,0 +1,16 @@
+
+export default {
+    default: "#E0E0E0",
+    defaultHover: "#AEAEAE",
+    defaultText: "#3F3F3F",
+    primary: "#2962FF",
+    primaryHover: "#0039CB",
+    secondary: "#455A64",
+    secondaryHover: "#1C313A",
+    danger: "#D32F2F",
+    dangerHover: "#9A0007",
+    disabled: "#E0E0E0",
+    disabledText: "#9E9E9E",
+
+    dropShadow: "rgba(51, 51, 51, 0.2)"
+}
